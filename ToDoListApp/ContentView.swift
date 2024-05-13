ddcee0190e01b8c13564504b4bbd1663df479ee9
Haily San Cesario <hailysanc04@gmail.com>
2024-05-12 20:11:16 -0400

    Iteration 1 Done!! Had to commit Conent view.

--- ToDoListApp/ContentView.swift
+++ ToDoListApp/ContentView.swift
@@ -8,14 +8,47 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State var toDoItems: [ToDoItem] = []
+    @State private var showNewTask = false
+    //whether we should show the screen or not
+    
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, Scholar!")
+            HStack {
+                Text("My To Do List")
+                    .font(.system(size: 35))
+                    .fontWeight(.black)
+                    .foregroundColor(Color.purple)
+                Spacer()
+                
+                Button(action: {
+                    self.showNewTask = true
+                }) {
+                    Text("+")
+                        .fontWeight(.bold)
+                        .foregroundColor(Color(hue: 0.694, saturation: 0.672, brightness: 0.646))
+                }
+                
+                Spacer()
+            }
+            .padding()
+            
         }
         .padding()
+        
+        List {
+                ForEach (toDoItems) { toDoItem in
+                    if toDoItem.isImportant == true {
+                        Text("‼️" + toDoItem.title + "‼️")
+                    } else {
+                        Text(toDoItem.title)
+                    }
+                    }
+        }.listStyle(.plain)
+        
+        if showNewTask {
+            NewToDoView(title: "", isImportant: false, toDoItems: $toDoItems, showNewTask: $showNewTask)
+        }
     }
 }
 
--- ToDoListApp/NewToDoView.swift
+++ ToDoListApp/NewToDoView.swift
@@ -8,11 +8,53 @@
 import SwiftUI
 
 struct NewToDoView: View {
+    
+    @State var title: String
+    @State var isImportant: Bool
+    @Binding var toDoItems: [ToDoItem]
+    @Binding var showNewTask : Bool
+    
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack {
+            Text("Task title:")
+                .font(.title)
+                .fontWeight(.bold)
+                .foregroundColor(Color(hue: 0.88, saturation: 0.52, brightness: 0.973))
+            TextField("Task Description Goes Here...", text: $title)
+                    .padding()
+                .background(Color(.systemGroupedBackground))
+                .cornerRadius(15)
+                  .padding()
+            Toggle(isOn: $isImportant) {
+                Text("Is it Important?")
+                    .font(.title3)
+                    .fontWeight(.heavy)
+                    .foregroundColor(Color(hue: 1.0, saturation: 0.583, brightness: 0.868))
+            }.padding()
+            
+            Button(action: {
+                self.addTask(title: self.title, isImportant: self.isImportant)
+                self.showNewTask = false
+            }) {
+                Text("Add")
+            }.padding()
+        }
+    }
+    
+    private func addTask(title: String, isImportant: Bool = false) {
+            
+            let task = ToDoItem(title: title, isImportant: isImportant)
+            toDoItems.append(task)
     }
 }
 
-#Preview {
-    NewToDoView()
+//#Preview {
+//    NewToDoView()
+//}
+
+struct NewToDoView_Previews: PreviewProvider {
+    static var previews: some View {
+        NewToDoView(title: "", isImportant: false, toDoItems: .constant([]), showNewTask: .constant(true))
+    }
 }
